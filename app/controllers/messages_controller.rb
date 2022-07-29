4b019ce1473b4bddfdfb4295d1da4befb2866222
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2022-07-29 20:46:39 +0200

    messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,32 @@
+class MessagesController < ApplicationController
+  
+  def create
+    @application = Application.find_by(token:  params[:application_id])
+    if !@application
+      return render json: { :message => "application not found" }.to_json, status: :not_found
+    end
+
+    @chat = Chat.find_by(application: @application.id, number: params[:chat_id])
+
+    if !@chat
+      return render json: { :message => "chat not found" }.to_json, status: :not_found
+    end
+    number = get_message_number(@chat.id)
+ 
+    @chat.messages.create(number: number, body: params[:body])
+
+    render json: {message_number: number}, status: :created
+
+  end
+
+  def get_message_number chat_id
+    Chat.transaction do
+      chat = Chat.lock.find(chat_id)
+      message_number = chat.next_message_number
+      chat.next_message_number += 1
+      chat.save!
+      message_number
+    end
+  end
+  
+end
